ac9f830476743c6a4af0dc66af74144489f17f6c
Bryan Chapman <bryan.chapmanbb@gmail.com>
2025-06-21 21:27:34 -0700

    Initial commit

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,45 @@
+use rand::Rng;
+use std::{cmp::Ordering, num::ParseIntError, io};
+
+fn main() {
+    println!("Guess the number...");
+
+    // reset guesses and create secret number
+    let secret_number= initiatize_game();
+    let mut guesses: u32 = 0; 
+
+    loop {
+        // ask for guess and validate int
+        let guess: u32 = match prompt_guess() {
+            Ok(guess) => guess,
+            Err(_) => continue,
+        };
+
+        // increment guesses, input was valid
+        guesses += 1;
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess was too small!"),
+            Ordering::Greater => println!("Your guess was too big!"),
+            Ordering::Equal => {
+                println!("You win in {} guesses!", guesses);
+                break;
+            }
+        }
+    }
+}
+
+fn initiatize_game() -> u32 {
+    return rand::rng().random_range(1..=100);
+}
+
+fn prompt_guess() -> Result<u32, ParseIntError>  {
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    return guess.trim().parse();
+}
